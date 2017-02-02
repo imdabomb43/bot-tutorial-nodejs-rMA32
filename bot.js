94ebb8539baa7c85238988f0a0bcdb359471a36e
imdabomb43 <vic.schissler@gmail.com>
2017-02-02 12:46:29 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -109,7 +109,7 @@ function respond() {
   else if(request.text && botRegexSC.test(request.text)) {
     this.res.writeHead(200);
     
-    postMessage("http://daddyleagues.com/revival/team/"+request.text.substring(5,8)+"/schedule");
+    postMessage("http://daddyleagues.com/pure/team/"+request.text.substring(5,8)+"/schedule");
     this.res.end();
   }
   else if(request.text && botRegexP.test(request.text)) {
